e0eab399a99d3d1bda35e0b5efa49954b7f68a7b
tugalsan karabacak <tugalsan@gmail.com>
2023-01-31 10:32:58 +0300

    .

--- sync/sync_me.bat
+++ sync/sync_me.bat
@@ -1,5 +1,4 @@
-robocopy c:\me\ \\192.168.5.15\d\me\ /MIR /XF *.PST *.TMP *.VHDX
-robocopy \\192.168.5.15\d\xampp_data\ D:\xampp\xampp_data\ /MIR
-robocopy \\192.168.5.15\d\xampp\htdocs\ D:\xampp\htdocs\ /MIR
+robocopy c:\me\ \\192.168.5.15\b\ddrive\me\ /MIR /XF *.PST *.TMP *.VHDX
+robocopy \\192.168.5.15\b\ddrive\xampp_data\ D:\xampp\xampp_data\ /MIR
 
 pause
