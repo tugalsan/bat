985b5e75889b86066fa01b6a3011cb2faadf826e
tugalsan karabacak <tugalsan@gmail.com>
2023-05-31 09:42:37 +0300

    Update sync_me.bat

--- sync/sync_me.bat
+++ sync/sync_me.bat
@@ -1,4 +1,4 @@
-robocopy c:\me\ \\192.168.5.15\b\ddrive\me\ /MIR /XF *.PST *.TMP *.VHDX
-robocopy \\192.168.5.15\b\ddrive\xampp_data\ D:\xampp\xampp_data\ /MIR
+robocopy c:\me\ \\192.168.5.15\b\ddrive\me\ /MIR /XF *.PST *.TMP *.VHDX *.WAR /XD target
+robocopy D:\xampp\xamppb_data\ \\192.168.5.15\d\xampp_data\ /MIR
 
 pause
